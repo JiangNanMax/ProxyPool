897ed92f735e7116a78ddf27e61836b04d505f07
jiangnan <18918020032@163.com>
2018-12-09 22:55:34 +0800

    完成scheduler模块

--- proxypool/scheduler.py
+++ proxypool/scheduler.py
@@ -61,35 +61,56 @@ class ProxyPoolAdder(object):
         self._tester = ValidtyTester()
         self._upper_threshold = upper_threshold
 
-    def if_over_upper_threshold(self):
+    def is_over_upper_threshold(self):
         return self._conn.list_len >= self._upper_threshold
 
     def add_proxies_to_pool(self):
         print("Running the ProxyPoolAdder...")
+        raw_proxies_count = 0
+        while not self.is_over_upper_threshold():
+            for callback_type in range(self._crawler.__CrawlFuncCount__):
+                callback = self._crawler.__CrawlFunc__[callback_type]
+                raw_proxies = self._crawler.get_raw_proxies(callback=callback)
+                self._tester.set_raw_proxies(raw_proxies)
+                self._tester.test_all_proxies()
+                raw_proxies_count += len(raw_proxies)
+                if self.is_over_upper_threshold():
+                    print("Proxies are enough...")
+                    break
+            if raw_proxies_count == 0:
+                raise ProxyResourceDepletionError
 
 
 class Scheduler(object):
-    pass
+    @staticmethod
+    def test_proxies(cycle=PROXY_VALID_CHECK_CYCLE):
+        conn = RedisClient()
+        tester = ValidtyTester()
+        while True:
+            print("Testing the validty of proxies...")
+            count_to_test = int(0.5 * conn.list_len)
+            if count_to_test == 0:
+                print("The proxy pool is empty...")
+                time.sleep(cycle)
+                continue
+            raw_proxies = conn.get_for_test(count_to_test)
+            tester.set_raw_proxies(raw_proxies)
+            tester.test_all_proxies()
+            time.sleep(cycle)
 
+    @staticmethod
+    def check_pool_len(lower_thershold=PROXYPOOL_LOWER_THRESHOLD, upper_threshold=PROXYPOOL_UPPER_THRESHOLD, cycle=PROXYPOOL_LEN_CHECK_CYCLE):
+        conn = RedisClient()
+        adder = ProxyPoolAdder(upper_threshold)
+        while True:
+            if conn.list_len < lower_thershold:
+                adder.add_proxies_to_pool()
+            time.sleep(cycle)
 
+    def run(self):
+        print("Running the scheduler...")
+        test_process = Process(target=Scheduler.test_proxies())
+        check_process = Process(target=Scheduler.check_pool_len())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        test_process.start()
+        check_process.start()
